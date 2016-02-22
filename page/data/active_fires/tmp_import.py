0e4dfa2f5b72384f973b8ed992c2eee7ac1dce66
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2016-02-22 16:41:55 -0500

    delete tmp file

--- page/data/active_fires/tmp_import.py
+++
@@ -1,4 +0,0 @@
-import os
-from page.data.active_fires.import_hotspots import modis
-from django.conf import settings
-modis(os.path.join(settings.BASE_DIR, 'page', 'data', 'active_fires', 'modis', 'files', 'South_America_MCD14DL_2014352.txt'))

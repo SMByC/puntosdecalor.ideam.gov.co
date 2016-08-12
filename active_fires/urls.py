cc57688085a9a09012e4ff7891b5e9232419a41f
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2016-08-12 16:35:27 -0500

    Fixes JavaScriptCatalog in Django 1.10

--- active_fires/urls.py
+++ active_fires/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.contrib.gis import admin
+from django.views.i18n import JavaScriptCatalog
 
 from page import views, static_hotspot_files
 from page.forms import Period
@@ -19,7 +20,7 @@ urlpatterns = [
     url(r'^data.geojson/(?P<from_year>[0-9]{4})-(?P<from_month>[0-9]{1,2})-(?P<from_day>[0-9]{1,2})/(?P<to_year>[0-9]{4})-(?P<to_month>[0-9]{1,2})-(?P<to_day>[0-9]{1,2})/$', views.ActiveFireMapLayer.as_view(model=ActiveFire, properties=('popup_text',)), name='data'),
 
     ###
-    url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', {'packages': ('es-CO',)}),
+    url(r'^jsi18n/$', JavaScriptCatalog.as_view(packages=['es-CO']), name='javascript-catalog'),
 
     ### for static ftp csv files of hostpot
     url(r'^ftp_files/(?P<path>.*)$', static_hotspot_files.serve, {'document_root': '/home/xavier/Projects/SMBYC/active_fires/static/ftp_files/', 'show_indexes': True}),

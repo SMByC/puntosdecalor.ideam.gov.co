7af4dad228a0c65d91fa5d0189602fcdd316ed66
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2016-08-19 13:02:36 -0500

    Several adjusted daterange widget

--- static/scripts/date.range.js
+++ static/scripts/date.range.js
@@ -4,18 +4,52 @@ $(function () {
         "autoApply": true,
         "maxDate": moment(),
         "opens": "left",
-        autoUpdateInput: true,
-        locale: {
-            format: 'YYYY-MM-DD'
-        },
+        "autoUpdateInput": true,
+        "linkedCalendars": false,
         "alwaysShowCalendars": true,
-        ranges: {
-            'Today': [moment(), moment()],
-            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-            'This Month': [moment().startOf('month'), moment().endOf('month')],
-            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        "locale": {
+            "format": 'YYYY-MM-DD',
+            "separator": " - ",
+            "applyLabel": "Aplicar",
+            "cancelLabel": "Cancelar",
+            "fromLabel": "De",
+            "toLabel": "A",
+            "customRangeLabel": "Personalizado",
+            "weekLabel": "S",
+            "daysOfWeek": [
+                "Do",
+                "Lu",
+                "Ma",
+                "Mi",
+                "Ju",
+                "Vi",
+                "Sa"
+            ],
+            "monthNames": [
+                "Enero",
+                "Febrero",
+                "Marzo",
+                "Abril",
+                "Mayo",
+                "Junio",
+                "Julio",
+                "Agosto",
+                "Septiembre",
+                "Octubre",
+                "Noviembre",
+                "Diciembre"
+            ],
+            "firstDay": 1
+        },
+        "ranges": {
+            'Hoy': [moment(), moment()],
+            'Desde 1 día atrás': [moment().subtract(1, 'days'), moment()],
+            'Desde 3 días atrás': [moment().subtract(3, 'days'), moment()],
+            'Desde 8 días atrás': [moment().subtract(8, 'days'), moment()],
+            'Desde 15 días atrás': [moment().subtract(15, 'days'), moment()],
+            'Mes actual': [moment().startOf('month'), moment().endOf('month')],
+            'Desde 1 mes atrás': [moment().subtract(1, 'month').startOf('month'), moment()],
+            'Desde 2 mes atrás': [moment().subtract(2, 'month').startOf('month'), moment()]
         }
     });
 

709c857a6126ab1a3948a07235a94d8b708b167e
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2017-08-07 22:19:50 -0500

    Add dns to allowed host

--- active_fires/settings.py
+++ active_fires/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'm%tfjngc5i%%6m*mlt!oz_+l38ku+udds*^-j)bc=@2hd3zt!a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['incendiosactivos.ideam.gov.co']
 
 # Application definition
 

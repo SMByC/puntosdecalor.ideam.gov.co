3ab472114a7ba0ae714a0c954667d12f88c2a2ff
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2017-08-14 16:52:43 -0500

    Migrate to new url format

--- active_fires/urls.py
+++ active_fires/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.contrib.gis import admin
+from django.views.i18n import javascript_catalog
 
 from page import views, static_hotspot_files
 from page.forms import Parameters
@@ -20,7 +21,7 @@ urlpatterns = [
     url(r'^data.geojson/$', views.ActiveFireMapLayer.as_view(model=ActiveFire, properties=('popup_text',)), name='data'),
 
     ###
-    url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', {'packages': ('es-CO',)}),
+    url(r'^jsi18n/$', javascript_catalog, {'packages': ('es-CO',)}),
 
     ### for static ftp csv files of hostpot
     url(r'^ftp_files/(?P<path>.*)$', static_hotspot_files.serve, {'document_root': '/home/xavier/Projects/SMBYC/active_fires/static/ftp_files/', 'show_indexes': True}),

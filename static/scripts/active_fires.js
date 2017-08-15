b73a44f0c92a2bbe940a45042df112afd5d10729
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2017-08-14 23:41:15 -0500

    Fix little error in jquery

--- static/scripts/active_fires.js
+++ static/scripts/active_fires.js
@@ -6,15 +6,11 @@
 
 jQuery(document).ready(function($){
 
-    var mantenimiento = false;
-    if (mantenimiento) {
-        $("#warning-message").show();
-    }
     //MENU-HEADER
     //activacion del submenu del menu principal de navegacion
     var sublink_activated = false;
     $("div#header-menu").find("li").each(function() {
-        if ($(this).find("a").attr('href').replace('/','') == window.location.href.split("/").reverse()[1]) {
+        if ( (typeof $(this).find("a").attr('href') != 'undefined') && ($(this).find("a").attr('href').replace('/','') == window.location.href.split("/").reverse()[1]) ) {
             $(this).addClass("menuOn");
             sublink_activated = true;
         } else {

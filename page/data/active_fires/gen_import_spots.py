16ea2350d49412a7648e840b999cf2f11f737541
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2017-08-15 19:06:31 -0500

    Added script for import spots

---
+++ page/data/active_fires/gen_import_spots.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#
+# use:
+#
+# cd /home/activefires/apps/Active_Fires/page/data/active_fires
+# bash import.sh
+
+
+import pandas as pd
+
+
+date_range = pd.date_range("2016-01-01", "2017-08-15", freq="D")
+
+f = open('import.sh', 'w')
+for date_import in date_range:
+
+    f.write("python3.6 download_active_fires.py -s modis -d \"{}\"\n".format(date_import.strftime("%Y-%m-%d")))
+    f.write("python3.6 download_active_fires.py -s viirs -d \"{}\"\n".format(date_import.strftime("%Y-%m-%d")))
+
+f.close()

aeae2254439878b577c839d52c0fb0f8e875fd0f
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2018-03-07 17:48:13 -0500

    Delete unused code

--- active_fires/urls.py
+++ active_fires/urls.py
@@ -6,11 +6,8 @@
 
 from django.conf.urls import url
 from django.contrib.gis import admin
-from django.views.i18n import javascript_catalog
 
 from page import views, static_hotspot_files
-from page.forms import Parameters
-from djgeojson.views import GeoJSONLayerView
 from page.models import ActiveFire
 
 urlpatterns = [
@@ -25,9 +22,6 @@ urlpatterns = [
     url(r'^new-parameters/$', views.new_parameters, name='new_parameters'),
     # send data through ajax with geojson
     url(r'^data.geojson/$', views.ActiveFireMapLayer.as_view(model=ActiveFire, properties=('popup_text',)), name='data'),
-
-    ###
-    url(r'^jsi18n/$', javascript_catalog, {'packages': ('es-CO',)}),
 
     ### for static ftp csv files of hostpot
     url(r'^ftp_files/(?P<path>.*)$', static_hotspot_files.serve, {'document_root': '/home/activefires/apps/Active_Fires/page/data/ftp_files', 'show_indexes': True}),

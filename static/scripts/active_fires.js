2a5da548bef52200a1db2962e31dabf2954ab3aa
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2018-04-09 19:52:07 -0500

    Added function for update url

--- static/scripts/active_fires.js
+++ static/scripts/active_fires.js
@@ -1,5 +1,37 @@
+//  (c) Copyright IDEAM-2018
+//  Authors: Xavier Corredor Ll. <xcorredorl@ideam.gov.co>
+
 //FUCTIONS----------------------------------------------------------
 
+// function for change get parameters
+function updateQueryStringParameter(uri, key, value) {
+    // use value=undefined for delete parameter
+    var i = uri.indexOf('#');
+    var hash = i === -1 ? '' : uri.substr(i);
+    uri = i === -1 ? uri : uri.substr(0, i);
+
+    var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+
+    if (!value) {
+        // remove key-value pair if value is empty
+        uri = uri.replace(new RegExp("([?&]?)" + key + "=[^&]*", "i"), '');
+        if (uri.slice(-1) === '?') {
+            uri = uri.slice(0, -1);
+        }
+        // replace first occurrence of & by ? if no ? is present
+        if (uri.indexOf('?') === -1) uri = uri.replace(/&/, '?');
+    } else if (uri.match(re)) {
+        uri = uri.replace(re, '$1' + key + "=" + value + '$2');
+    } else {
+        uri = uri + separator + key + "=" + value;
+    }
+    return uri + hash;
+}
+
+function updateUrlParameter(key, value) {
+    window.history.pushState('', '', updateQueryStringParameter(window.location.search, key, value));
+}
 
 //==============================================================================
 //run when DOM is ready

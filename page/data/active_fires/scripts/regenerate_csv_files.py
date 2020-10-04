b551ffdaa33b452311850d71ca375be8f1cb2e66
XavierCLL <xavier.corredor.llano@gmail.com>
2020-10-04 14:38:39 -0700

    Regeneration specific by date

--- page/data/active_fires/scripts/regenerate_csv_files.py
+++ page/data/active_fires/scripts/regenerate_csv_files.py
@@ -11,3 +11,10 @@ from page.data.active_fires.import_active_fires import save_in_csv_table
 for item in ActiveFire.objects.all().order_by('date', 'source'):
     save_in_csv_table(item)
 
+# specific for date  WARNING: comment for run as script
+
+from_datetime = datetime.strptime("2020-10-04 00:00:00", "%Y-%m-%d %H:%M:%S")
+to_datetime = datetime.strptime("2020-10-04 23:59:59", "%Y-%m-%d %H:%M:%S")
+
+for item in ActiveFire.objects.filter(date__gte=from_datetime, date__lte=to_datetime).order_by('date', 'source'):
+    save_in_csv_table(item)

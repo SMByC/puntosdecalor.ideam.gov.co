6fd09917a7e1df6f95fa0ad65de0369ba6e6897c
XavierCLL <xavier.corredor.llano@gmail.com>
2020-10-04 13:49:46 -0700

    Fix the generation script

--- page/data/active_fires/scripts/regenerate_csv_files.py
+++ page/data/active_fires/scripts/regenerate_csv_files.py
@@ -1,9 +1,12 @@
 from datetime import datetime, date
 from page.models import ActiveFire
 
-from ..import_active_fires import save_in_csv_table
+from page.data.active_fires.import_active_fires import save_in_csv_table
 
 ### Regenerate all CSV files
+#
+# to run:
+# python3.8 manage.py shell < page/data/active_fires/scripts/regenerate_csv_files.py
 
 for item in ActiveFire.objects.all().order_by('date', 'source'):
     save_in_csv_table(item)

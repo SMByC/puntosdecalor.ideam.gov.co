0a2cdc1432208d2420eda855749600952850a0c3
XavierCLL <xavier.corredor.llano@gmail.com>
2020-10-03 23:36:16 -0700

    Fix the time zones, always use the Colombian time!

--- active_fires/settings.py
+++ active_fires/settings.py
@@ -76,13 +76,13 @@ DATABASES = {
 
 LANGUAGE_CODE = 'es-CO'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/

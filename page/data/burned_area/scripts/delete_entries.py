a55b3d116f0bbd884fd9949d57706b352f0b086a
Xavier C. Llano <xavier.corredor.llano@gmail.com>
2022-11-06 12:39:54 -0800

    Update modis path

--- page/data/burned_area/scripts/delete_entries.py
+++ page/data/burned_area/scripts/delete_entries.py
@@ -4,8 +4,8 @@ from page.models import BurnedArea
 
 ### Delete specific entries
 
-from_datetime = datetime.strptime("2020-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
-to_datetime = datetime.strptime("2020-12-31 23:59:59", "%Y-%m-%d %H:%M:%S")
+from_datetime = datetime.strptime("2021-05-01 00:00:00", "%Y-%m-%d %H:%M:%S")
+to_datetime = datetime.strptime("2021-06-30 23:59:59", "%Y-%m-%d %H:%M:%S")
 
 burned_area = BurnedArea.objects.filter(date__gte=from_datetime, date__lte=to_datetime)
 

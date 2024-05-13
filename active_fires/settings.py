9ed2fb69ac21fd0e9424190ca263bbf90a7cc2ff
Xavier C. Llano <xavier.corredor.llano@gmail.com>
2024-05-12 20:21:59 -0700

    Fix allowed hosts

--- active_fires/settings.py
+++ active_fires/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'm%tfjngc5i%%6m*mlt!oz_+l38ku+udds*^-j)bc=@2hd3zt!a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['incendiosactivos.ideam.gov.co', 'puntosdecalor.ideam.gov.co']
+ALLOWED_HOSTS = ['.puntosdecalor.ideam.gov.co/', '127.0.0.1', 'localhost']
 
 # Application definition
 
